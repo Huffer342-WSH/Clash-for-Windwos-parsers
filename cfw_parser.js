dcef3ec6849b0276a471f0385b80920296f376fe
Huffer342-WSH <718007138@qq.com>
2024-06-19 16:02:15 +0800

    Matlab

--- cfw_parser.js
+++ cfw_parser.js
@@ -354,6 +354,10 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
 
     //////////////////////  rules  //////////////////////
     const newRules = [
+        //Matlab
+        'DOMAIN,esd.mathworks.com,DIRECT',
+        'PROCESS-NAME,MATLABWindow.exe,DIRECT',
+        'DOMAIN-SUFFIX,mathworks.com,默认代理',
 
         //IEEE
         'DOMAIN,ieeexplore.ieee.org,默认代理',
@@ -391,7 +395,7 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
         'DOMAIN-SUFFIX,pythonhosted.org,默认代理',
         'DOMAIN-SUFFIX,codeium.com,默认代理',
         'DOMAIN-SUFFIX,hp.com,默认代理',
-        'DOMAIN-KEYWORD,mathworks,默认代理',
+
         'DOMAIN-SUFFIX,acg.rip,默认代理',
         'DOMAIN-SUFFIX,sublimetext.com,默认代理',
         'DOMAIN-SUFFIX,zmyos.com,默认代理',
