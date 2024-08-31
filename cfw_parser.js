edebf14e81be270b102d319bca866ebd20751abc
Huffer342-WSH <718007138@qq.com>
2024-08-31 16:15:16 +0800

    amazonaws.com

--- cfw_parser.js
+++ cfw_parser.js
@@ -265,6 +265,7 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
         'PROCESS-NAME,steam.exe,DIRECT',
 
         //默认代理
+        'DOMAIN-SUFFIX,amazonaws.com,默认代理',
         'DOMAIN,pypi.org,默认代理',
         'DOMAIN,conda.anaconda.org,默认代理',
         'DOMAIN,www.freertos.org,默认代理',
