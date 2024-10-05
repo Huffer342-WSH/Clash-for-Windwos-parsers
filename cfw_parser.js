a4c28fc9947d41ecadba7cbbbb89bfa0cff76e9e
Huffer342-WSH <718007138@qq.com>
2024-10-05 14:19:15 +0800

    DOMAIN-SUFFIX,entitlenow.com,DIRECT

--- cfw_parser.js
+++ cfw_parser.js
@@ -321,7 +321,7 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
         'DOMAIN-SUFFIX,zmyos.com,默认代理',
 
         // DIRECT
-        'DOMAIN,xilinx-ax-dl.entitlenow.com,DIRECT',
+        'DOMAIN-SUFFIX,entitlenow.com,DIRECT',
         'DOMAIN-SUFFIX,cn.mm.bing.net,DIRECT',
         'DOMAIN,www.bing.com,DIRECT',
         'DOMAIN,cn.bing.com,DIRECT',
